247f9d27d1a7597b938936d2ab8f7f7f02b22381
Elkrose <elkrose.gm@gmail.com>
2026-02-10 21:49:55 -0500

    Update virgin_serums_ren.py
    fixed Smart Roses on Apply was forcing past 20

--- major_game_classes/serum_related/serums/virgin_serums_ren.py
+++ major_game_classes/serum_related/serums/virgin_serums_ren.py
@@ -317,7 +317,6 @@ def red_roses_on_turn(person: Person, serum: SerumDesign, add_to_log: bool):
             mc.log_event(f"{person.title or person.create_formatted_title('???')}:R3D R0535 getting her wet...", "float_text_pink")
     return
 def red_roses_on_remove(person: Person, serum: SerumDesign, add_to_log: bool):
-    #person.change_focus(2, add_to_log = add_to_log)
     if add_to_log:
         mc.log_event(f"{person.title or person.create_formatted_title('???')}: R3D R0535 Completed", "float_text_red")
     return
@@ -463,18 +462,21 @@ def smart_roses_on_apply(person: Person, serum: SerumDesign, add_to_log: bool):
     #first dose brings them up to base good stats
     if add_to_log:
         mc.log_event(f"{person.title or person.create_formatted_title('???')}: 5M4RT R053 injested", "float_text_blue")
-    if person.charisma < 4:
-        person.charisma = 4
-    else:
-        person.charisma +=1
-    if person.int < 4:
-        person.int = 4
-    else:
-        person.int +=1
-    if person.focus < 4:
-        person.focus = 4
-    else:
-        person.focus +=1
+    if person.charisma <=19:
+        if person.charisma < 4:
+            person.charisma = 4
+        else:
+            person.charisma +=1
+    if person.int <=19:
+        if person.int < 4:
+            person.int = 4
+        else:
+            person.int +=1
+    if person.focus <=19:
+        if person.focus < 4:
+            person.focus = 4
+        else:
+            person.focus +=1
     return
 def smart_roses_on_turn(person: Person, serum: SerumDesign, add_to_log: bool):
     if person.charisma < 20:
@@ -782,3 +784,4 @@ def init_Virginal_Serum():
         exclude_tags = "Pregnancy",
         hidden_tag = ["Pregnancy", "Medical", "Unique"],
         mental_aspect = 7, physical_aspect = 7, sexual_aspect = 5, medical_aspect = 9, flaws_aspect = 0, attention = 4)
+
